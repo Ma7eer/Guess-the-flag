4238a29e5ba0444fcac28c4f2e915de5f87c436a
Ma7eer <maher.alkendi2@gmail.com>
2018-11-13 07:49:18 -0700

    replaced radio inputs with buttons

--- src/App.js
+++ src/App.js
@@ -16,11 +16,11 @@ class App extends Component {
     super(props);
     this.state = {
       flag: null,
-      answer: null,
       answered: false,
       country: '',
       isCorrect: '',
-      score: 0
+      score: 0,
+      userAnswer: null
     }
   }
 
@@ -64,15 +64,10 @@ class App extends Component {
     });
   }
 
-  handleRadioChange = (event) => {
-    this.setState({
-      answer: event.target.value
-    });
-  }
-
-  handleRadioSubmit = (event) => {
+  handleButtonSubmit = (event) => {
     event.preventDefault();
-    if (this.state.answer.toLowerCase() === countryName.toLowerCase()) {
+    console.log(this.state.userAnswer);
+    if (this.state.userAnswer.toLowerCase() === countryName.toLowerCase()) {
       if (this.state.score === 90) {
         this.setState(prevState => {
           return {
@@ -94,6 +89,10 @@ class App extends Component {
     this.setState({answered: true})
   }
 
+  handleButtonClick = (event) => {
+    this.setState({userAnswer: event.target.value})
+  }
+
 
   render() {
     return (
@@ -104,13 +103,10 @@ class App extends Component {
         <div className="score-bar" style={{width: this.state.score + '%'}}><span className="score">{this.state.score + '%'}</span></div>
         </div>
         <Flag startNewGame={this.startNewGame} flagUrl={this.state.flag}/>
-        <form onSubmit={this.handleRadioSubmit}>
-          <div onChange={this.handleRadioChange}>
-          {quizChoices.map((ans, i) =>
-              {return this.state.answered ? (<span key={i}><input value={ans} type="radio" name="country" key={i} disabled /> {ans} </span>) : (<span key={i}><input value={ans} type="radio" name="country" key={i} /> {ans} </span>)}
-            )}
-          </div>
-          <button type="submit" className="btn">Submit</button>
+        <form onSubmit={this.handleButtonSubmit}>
+          {quizChoices.map((choice, i) => {
+            return this.state.answered ? <button onClick={this.handleButtonClick} type="submit" key={i} value={choice} disabled>{choice}</button> : <button onClick={this.handleButtonClick} type="submit" key={i} value={choice} >{choice}</button>
+          })}
         </form>
         {this.state.isCorrect === 'Correct!' ? <div>
         <button onClick={this.goToNextQuestion}>Next</button> <h1 style={{color:'green'}}>{this.state.isCorrect}</h1>
